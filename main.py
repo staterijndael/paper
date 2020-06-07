edf82c8f348c2dcad3656a69b7de6bfbb56eff4e
Oringik <zikalol2@mail.ru>
2020-06-07 12:42:43 +0500

    fixed cli

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import click
  
-image = Image.open('gazette.jpg').convert("RGBA")
 
 count = 1
 deep = 0
@@ -13,12 +12,7 @@ text_color = (0,0,0)
  
     
 
-(width, height) = image.size
-
-
-draw = ImageDraw.Draw(image)
-
-def addNewLayer(imageLayer, description):
+def addNewLayer(imageLayer, description, width, height, draw, image):
     im = Image.open(imageLayer).convert("RGBA")
     im.thumbnail((200, 200))
     font = ImageFont.truetype('arial.ttf', 15)
@@ -30,20 +24,21 @@ def addNewLayer(imageLayer, description):
 
     global count
     global deep
-    if count != 3:
-        x_pos = count * 200 + count * 130 - 240
-        y_pos = 180 + 500*deep
+    if count != 4:
+        x_pos =  0.3 * width * count - 230
+        y_pos = 0.32 * deep * height + 200
         yt_pos = y_pos + 220
         count = count + 1
-    elif count == 3:
-        x_pos = count * 200 + count * 130 - 240
-        y_pos = 180 + 500 * deep
-        yt_pos = 220 + 180 + 500*deep
-        count = 1
+    else:
+        count = 2
         deep = deep + 1
+        x_pos =  0.3 * width - 230
+        y_pos = 0.32 * deep * height + 200
+        yt_pos = y_pos + 220
+
     img_with_border = ImageOps.expand(im,border=10,fill='black')
-    draw.text((x_pos, yt_pos), description, text_color, font)
-    image.paste(img_with_border, (x_pos,y_pos), img_with_border)
+    draw.text((int(x_pos), int(yt_pos)), description, text_color, font)
+    image.paste(img_with_border, (int(x_pos),int(y_pos)), img_with_border)
     image.save("greeting_card.png")
         
 
@@ -54,9 +49,10 @@ def addNewLayer(imageLayer, description):
 @click.option('--title', default='', help="Title of the paper.")
 @click.option('--images',multiple=True, help="Some images to insert into paper.")
 @click.option('--descs', multiple=True, help='Some descs to attach to images to insert into paper.')
-def cli(title,images,descs):
-    if not images or not descs or not title:
-        click.echo("Images and descs is required params")
+@click.option('--bg',default='', help='Set background for the school newspaper .')
+def cli(title,images,descs,bg):
+    if not images or not descs or not title or not bg:
+        click.echo("Images,background and descs is required params")
         return
     if len(images) != len(descs):
         click.echo("Images count must be equals to descs count")
@@ -64,15 +60,18 @@ def cli(title,images,descs):
 
 
  
+    image = Image.open(bg).convert("RGBA")
+
+    (width, height) = image.size
 
 
-
+    draw = ImageDraw.Draw(image)
 
     font = ImageFont.truetype('impact.ttf', size=45)
  
 
  
-    (x, y) = (400, 50)
+    (x, y) = (0.28*width, 0.03*height)
     color = 'rgb(0, 0, 0)' 
  
 
@@ -90,6 +89,10 @@ def cli(title,images,descs):
         stay = stay.resize((200,200), Image.ANTIALIAS)
         stay.save(images[i], "JPEG")
         print(images[i])
-        addNewLayer(images[i], descs[i])
+        addNewLayer(images[i], descs[i], width, height, draw, image)
         i = i + 1
+        
+        
+if __name__ == "__main__":
+    cli()
 
